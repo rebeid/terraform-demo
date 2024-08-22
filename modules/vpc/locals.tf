eb06738c99f586637d77f50bc993707e8d29ceb2
Rina Ebeid <rina.ebeid@shipwire.com>
2024-08-22 00:51:19 -0700

    removed vpc module and updated gke module to include vpc creation

--- modules/vpc/locals.tf
+++
@@ -1,7 +0,0 @@
-locals{
-  dev   = var.env == "dev" ? "10" : ""
-  stage = var.env == "stage" ? "20" : ""
-  prod  = var.env == "prod" ? "30" : ""
-  octet = coalesce(local.dev, local.stage, local.prod)
-  subnet_ip = "10.0.${local.octet}.0/24"
-}
